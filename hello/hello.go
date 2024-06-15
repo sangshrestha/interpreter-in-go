ce03be451755a9b518f91d28f3523d6ff7a6cded
sangshrestha <sang.socials@gmail.com>
2024-06-15 18:22:06 +0100

    feat: handle errors

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		errMessage := fmt.Sprintf("Err: %v", err)
+		log.Fatal(errMessage)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 }
