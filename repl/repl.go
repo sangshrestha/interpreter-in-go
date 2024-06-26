8bd97ff7f33e36bc6720ef6823c76029d9f541b5
sangshrestha <sang.socials@gmail.com>
2024-06-26 21:02:23 +0100

    fix: dont use format print

--- repl/repl.go
+++ repl/repl.go
@@ -13,7 +13,7 @@ const PROMPT = ">> "
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	for {
-		fmt.Printf(PROMPT)
+		fmt.Print(PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
